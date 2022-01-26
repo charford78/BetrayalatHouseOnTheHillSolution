a04d0b4012b30fed387a7e69e766da01345d7e91
Charles Ford <charles.ford1978@gmail.com>
2022-01-26 17:50:32 -0500

    Attempted to add code so that user could enter the number of dice in the console and it would keep running until a valid number had been entered.  Still need to fix a couple of bugs.

--- BetrayalatHouseOnTheHill/Program.cs
+++ BetrayalatHouseOnTheHill/Program.cs
@@ -8,7 +8,28 @@ namespace BetrayalatHouseOnTheHill
         {
             var dice = new Dice();
 
-                dice.DiceRoller();
+            Console.WriteLine("Please enter the number of dice you must roll.  You cannot enter more than 8.");
+            var dicenum = Console.ReadLine();
+            var dnum = Convert.ToInt32(dicenum);
+
+            //public void DiceEnter()
+            //{
+            //    Console.WriteLine("Please enter the number of dice you must roll.  You cannot enter more than 8.");
+            //    dicenum = Console.ReadLine();
+            //}
+
+            //DiceEnter();
+
+            while(dnum !> 0 && dnum !<= 8)
+            {
+                if(Int32.TryParse(dicenum, out int num) && num > 0 && num <= 8)
+                {
+                    dice.DiceRoller(num);
+                } else
+                {
+                    Console.WriteLine("You must enter a number from 1 to 8!  Try Again.");
+                }
+            }
         }
     }
 }
